b5b22c65ccc5ef68633136841e455766ccab7b4b
hmkim829 <hmkim829@gmail.com>
2022-04-30 22:37:43 +0900

    Initial commit

---
+++ src/main/java/jpashop/StudyJpaShopApplication.java
@@ -0,0 +1,12 @@
+package jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyJpaShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyJpaShopApplication.class, args);
+	}
+}
