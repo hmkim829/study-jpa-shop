3ea5e8c1d23d1880e8f391cb6323d451ed814e89
hmkim829 <hmkim829@gmail.com>
2022-05-02 00:18:27 +0900

    엔터티 설계

--- src/test/java/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpashop/MemberRepositoryTest.java
@@ -1,11 +1,9 @@
 package jpashop;
 
-import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,17 +15,7 @@ public class MemberRepositoryTest {
 
     @Test
     @Transactional
-    @Rollback(false)
     public void testMember(){
 
-        Member member = new Member();
-        member.setUsername("memberA");
-
-        Long savedId = memberRepository.save(member);
-        Member findMember = memberRepository.find(savedId);
-
-        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
-        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
-        Assertions.assertThat(findMember).isEqualTo(member);
     }
 }
