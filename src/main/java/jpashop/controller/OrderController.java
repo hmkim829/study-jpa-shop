f539bc130dfd118d2fe42099546af1e17f542244
hmkim829 <hmkim829@gmail.com>
2022-05-12 00:35:20 +0900

    주문 목록 검색, 취소

--- src/main/java/jpashop/controller/OrderController.java
+++ src/main/java/jpashop/controller/OrderController.java
@@ -1,7 +1,9 @@
 package jpashop.controller;
 
 import jpashop.domain.Member;
+import jpashop.domain.Order;
 import jpashop.domain.item.Item;
+import jpashop.repository.OrderSearch;
 import jpashop.service.ItemService;
 import jpashop.service.MemberService;
 import jpashop.service.OrderService;
@@ -9,6 +11,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -38,6 +42,19 @@ public class OrderController {
                         @RequestParam("itemId") Long itemId,
                         @RequestParam("count") int count){
         orderService.order(memberId, itemId, count);
+        return "redirect:/orders";
+    }
+
+    @GetMapping("/orders")
+    public String orderList(@ModelAttribute("orderSearch") OrderSearch orderSearch, Model model){
+        List<Order> orders = orderService.findOrders(orderSearch);
+        model.addAttribute("orders", orders);
+        return "order/orderList";
+    }
+
+    @PostMapping("/orders/{orderId}/cancel")
+    public String cancel(@PathVariable("orderId") Long orderId){
+        orderService.cancelOrder(orderId);
         return "redirect:/orders";
     }
 }
