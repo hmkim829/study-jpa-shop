147fbbf6256c4ee9fe5cdde59749bc87e5fa6513
hmkim829 <hmkim829@gmail.com>
2022-05-15 01:20:41 +0900

    주문 조회 v1 - 엔터티 직접 노출

---
+++ src/main/java/jpashop/api/OrderApiController.java
@@ -0,0 +1,30 @@
+package jpashop.api;
+
+import jpashop.domain.Order;
+import jpashop.domain.OrderItem;
+import jpashop.repository.OrderRepository;
+import jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/orders")
+    public List<Order> orderV1(){
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.stream().forEach(o->o.getItem().getName());
+        }
+        return all;
+    }
+}
